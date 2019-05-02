bc8a71a723ebb9905f99da2e4028b1e8a876d3ff
Hotjam <hotjam@live.com.pt>
2019-05-02 18:18:35 +0100

    primeiros progressos

--- cliente.java
+++ cliente.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 class Cliente {
 
@@ -10,19 +11,29 @@ class Cliente {
   private static int porta = 0;
   private static String hostname = "";
   private static String fileName = "";
+  private static int retransmitted = 0;
   private static Scanner input = new Scanner(System.in);
   private static Socket s = null;
-  
-  public static void main(String[] args) throws IOException{
 
-    System.out.println("username=");
+
+  public static void main(String[] args) throws IOException, InterruptedException{
+
+    System.out.println("username");
     username = input.nextLine();
-    System.out.println("password=");
+    System.out.println("password");
     password = input.nextLine();
 
+    while(!(username.equals("servercctp3") && password.equals("grupo64"))){
+      System.out.println("Credencias incorretas! \n ->> tente: usr = servercctp3 passwd = grupo64");
+      System.out.println("username");
+      username = input.nextLine();
+      System.out.println("password");
+      password = input.nextLine();
+    }
 
-    if(username.equals("servercctp3") && password.equals("grupo64")){
-      System.out.println("Autenticação efetuada com sucesso \n > A enviar pacote..");
+
+      System.out.println("Autenticação efetuada com sucesso \n > A processar pedido..");
+      TimeUnit.SECONDS.sleep(2);
 
       porta = Integer.parseInt(args[0]);
       hostname = args[1];
@@ -39,9 +50,89 @@ class Cliente {
       byte[] ficheiroByteArray = new byte[fileLenght];
 
       //enviar ficheiro
+      beginTransfer(socket, ficheiroByteArray, address);
 
+      System.out.println("> ficheiro " + fileName + " com sucesso!");
       socket.close();
-    }
   }
+
+    private static void beginTransfer(DatagramSocket socket, byte[] fileByteArray, InetAddress address) throws IOException {
+
+        int sequenceNumber = 0;
+        boolean flag;
+        int ackSequence = 0;
+
+        for (int i = 0; i < fileByteArray.length; i = i + 1021) {
+            sequenceNumber += 1;
+            // Create message
+            byte[] message = new byte[1024];
+            message[0] = (byte) (sequenceNumber >> 8);
+            message[1] = (byte) (sequenceNumber);
+
+            if ((i + 1021) >= fileByteArray.length) {
+                flag = true;
+                message[2] = (byte) (1);
+            } else {
+                flag = false;
+                message[2] = (byte) (0);
+            }
+
+            if (!flag) {
+                System.arraycopy(fileByteArray, i, message, 3, 1021);
+            } else { // If it is the last message
+                System.arraycopy(fileByteArray, i, message, 3, fileByteArray.length - i);
+            }
+
+            DatagramPacket sendPacket = new DatagramPacket(message, message.length, address, porta);
+
+            socket.send(sendPacket);
+
+            System.out.println("Sent: Sequence number = " + sequenceNumber);
+
+            // For verifying the the packet
+            boolean ackRec;
+
+            // The acknowledgment is not correct
+            while (true) {
+                // Create another packet by setting a byte array and creating
+                // data gram packet
+                byte[] ack = new byte[2];
+                DatagramPacket ackpack = new DatagramPacket(ack, ack.length);
+
+                try {
+                    // set the socket timeout for the packet acknowledgment
+                    socket.setSoTimeout(50);
+                    socket.receive(ackpack);
+                    ackSequence = ((ack[0] & 0xff) << 8)
+                            + (ack[1] & 0xff);
+                    ackRec = true;
+
+                }
+                // we did not receive an ack
+                catch (SocketTimeoutException e) {
+                    System.out.println("Socket timed out waiting for the ");
+                    ackRec = false;
+                }
+
+                // everything is ok so we can move on to next packet
+                // Break if there is an acknowledgment next packet can be sent
+                if ((ackSequence == sequenceNumber)
+                        && (ackRec)) {
+                    System.out.println("Ack received: Sequence Number = "
+                            + ackSequence);
+                    break;
+                }
+
+                // Re send the packet
+                else {
+                    socket.send(sendPacket);
+                    System.out.println("Resending: Sequence Number = "
+                            + sequenceNumber);
+                    // Increment retransmission counter
+                    retransmitted += 1;
+                }
+            }
+        }
+    }
 
 }
--- servidor.java
+++ servidor.java
@@ -7,34 +7,111 @@ public class servidor {
   private static int totalTransferred = 0;
   private static String fileName = "";
   private int id=0;
-  private HashMap<String, String> utilizadores; //<username, password>
+  //private HashMap<String, String> utilizadores; //<username, password>
 
-  public static void main(String[] args) throws IOException{
+  public static void main(String[] args) throws IOException, FileNotFoundException{
+
     int porta = Integer.parseInt(args[0]);
-
+    System.out.println("--SERVIDOR ATIVO--");
     //instancia socket para atribuir conexão
     DatagramSocket socketData = new DatagramSocket(porta);
-
-
-    while(true){
 
       //instancia do pacote a ser enviado
       byte[] fileNamePacket = new byte[1024];
       DatagramPacket filePacket = new DatagramPacket(fileNamePacket, fileNamePacket.length);
 
-      //
-      File file = new File(fileName);
-      FileOutputStream outToFile = new FileOutputStream(file);
-      System.out.println("FICHEIRO: " + fileName);
+      try{
+        File file = new File(fileName);
+        FileOutputStream outToFile = new FileOutputStream(file);
+        acceptTransfer(outToFile, socketData);
+        System.out.println("> A processar " + fileName + "...");
+      }
+      catch (FileNotFoundException f){
+        f.printStackTrace();
+      }
+
+
+      byte[] destFile = new byte[1024];
+      DatagramPacket destFilePacket = new DatagramPacket(destFile, destFile.length);
 
       socketData.receive(filePacket);
-      System.out.println("Ficheiro recebido com sucesso!");
 
-
-
-
+      System.out.println("Ficheiro" + fileName + "recebido com sucesso!");
       socketData.close();
-    }
   }
 
+  private static void acceptTransfer(FileOutputStream outToFile, DatagramSocket socket) throws IOException {
+
+        // last message flag
+        boolean flag;
+        int sequenceNumber = 0;
+        int findLast = 0;
+
+        while (true) {
+            byte[] message = new byte[1024];
+            byte[] fileByteArray = new byte[1021];
+
+            // Receive packet and retrieve message
+            DatagramPacket receivedPacket = new DatagramPacket(message, message.length);
+            socket.setSoTimeout(0);
+            socket.receive(receivedPacket);
+
+            message = receivedPacket.getData();
+            totalTransferred = receivedPacket.getLength() + totalTransferred;
+            totalTransferred = Math.round(totalTransferred);
+
+            // Get port and address for sending acknowledgment
+            InetAddress address = receivedPacket.getAddress();
+            int port = receivedPacket.getPort();
+
+            // Retrieve sequence number
+            sequenceNumber = ((message[0] & 0xff) << 8) + (message[1] & 0xff);
+            // Retrieve the last message flag
+            // a returned value of true means we have a problem
+            flag = (message[2] & 0xff) == 1;
+            // if sequence number is the last one +1, then it is correct
+            // we get the data from the message and write the message
+            // that it has been received correctly
+            if (sequenceNumber == (findLast + 1)) {
+
+                // set the last sequence number to be the one we just received
+                findLast = sequenceNumber;
+
+                // Retrieve data from message
+                System.arraycopy(message, 3, fileByteArray, 0, 1021);
+
+                // Write the message to the file and print received message
+                outToFile.write(fileByteArray);
+                System.out.println("Received: Sequence number:"
+                        + findLast);
+
+                // Send acknowledgement
+                sendAck(findLast, socket, address, port);
+            } else {
+                System.out.println("Expected sequence number: "
+                        + (findLast + 1) + " but received "
+                        + sequenceNumber + ". DISCARDING");
+                // Re send the acknowledgement
+                sendAck(findLast, socket, address, port);
+            }
+
+            // Check for last message
+            if (flag) {
+                outToFile.close();
+                break;
+            }
+        }
+    }
+
+    private static void sendAck(int findLast, DatagramSocket socket, InetAddress address, int port) throws IOException {
+        // send acknowledgement
+        byte[] ackPacket = new byte[2];
+        ackPacket[0] = (byte) (findLast >> 8);
+        ackPacket[1] = (byte) (findLast);
+        // the datagram packet to be sent
+        DatagramPacket acknowledgement = new DatagramPacket(ackPacket,
+                ackPacket.length, address, port);
+        socket.send(acknowledgement);
+        System.out.println("Sent ack: Sequence Number = " + findLast);
+    }
 }
